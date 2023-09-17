efcca387238d5ee7e79b083d41b05f76864768f2
sta.litvinenko <stepangg@yandex.ru>
2023-09-17 14:10:37 +0300

    cat implimentation"

--- TestTask/libs/cat/include/cat/cat.h
+++ TestTask/libs/cat/include/cat/cat.h
@@ -1,5 +1,19 @@
 #ifndef CAT_H
 #define CAT_H
 
+#include "animal/animal.h"
+
+#include <string>
+
+class Cat: public Animal{
+public:
+
+    Cat(std::string& str);
+    void say() override;
+   ~Cat() override;
+private:
+    std::string& outStr;
+};
+
 
 #endif
--- TestTask/libs/cat/source/cat.cpp
+++ TestTask/libs/cat/source/cat.cpp
@@ -1,1 +1,15 @@
 #include "cat/cat.h"
+
+
+
+Cat::Cat(std::string& str): outStr(str)
+{
+
+}
+
+void Cat::say()
+{
+    outStr = "Meow!";
+}
+
+Cat::~Cat()  = default;
